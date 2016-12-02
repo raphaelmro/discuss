ae7c9307f5e07a8e38141935cf12ffc3ec062a1d
Raphael Onofre <raphaelmoura@codata.pb.gov.br>
2016-12-02 17:02:53 -0300

    adjust in css, some routes created

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,6 +1,25 @@
 defmodule Discuss.TopicController do
     use Discuss.Web, :controller 
-    def new() do
-        
+    alias Discuss.Topic
+    
+    def index(conn, _params) do
+        topics = Repo.all(Topic)
+    end
+
+    def new(conn, _params) do
+        # struct = %Topic{}
+        # params = %{}
+        changeset = Topic.changeset(%Topic{}, %{})
+        render conn, "new.html", changeset: changeset
+    end
+
+    def create(conn, %{"topic" => topic}) do
+        changeset = Topic.changeset(%Topic{}, topic)
+
+        case Repo.insert(changeset) do
+            {:ok, post} -> IO.inspect(post)
+            {:error, changeset } -> 
+                render conn, "new.html", changeset: changeset
+        end
     end
 end
