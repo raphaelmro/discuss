ef9f138f4155cda8e6d0169ca47cc30cac334504
Raphael Onofre <raphaelmoura@codata.pb.gov.br>
2016-12-06 16:56:05 -0300

    OAuth setup

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }    
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.Oauth,
+  client_id: "d77cf1ed9e6ae263ce29",
+  client_secret: "956e26e06dedad4030394aa4c2538df5096d4e94"
