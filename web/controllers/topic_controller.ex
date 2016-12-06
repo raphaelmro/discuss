1c5a51295848cf431daae49ed943e8a632e4fd4f
Raphael Onofre <raphaelmoura@codata.pb.gov.br>
2016-12-06 15:50:13 -0300

    edit and delete ok

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -37,8 +37,8 @@ defmodule Discuss.TopicController do
     end
 
     def update(conn, %{"id" => topic_id, "topic" => topic}) do
-        changeset = Repo.get(Topic, topic_id) 
-        |>  Topic.changeset(topic)
+        old_topic = Repo.get(Topic, topic_id)
+        changeset = Topic.changeset(old_topic, topic)
 
         case Repo.update(changeset) do
             {:ok, _topic} ->
@@ -46,7 +46,19 @@ defmodule Discuss.TopicController do
                 |>  put_flash(:info, "Topic Updated")
                 |>  redirect(to: topic_path(conn, :index))
             {:error, changeset} ->
-                render conn, "edit.html"
+                render conn, "edit.html", 
+                changeset: changeset, 
+                topic: old_topic
         end
+    end
+
+    def delete(conn, %{"id" => topic_id}) do
+        Repo.get!(Topic, topic_id)
+        |>  Repo.delete!
+
+        conn
+        |>  put_flash(:info, "Topic Deleted")
+        |>  redirect(to: topic_path(conn, :index))
+
     end
 end
