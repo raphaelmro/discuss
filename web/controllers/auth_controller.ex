c6cd9b6bc0e75bebc88be726b7d74564b8ad668a
Raphael Onofre <raphaelmoura@codata.pb.gov.br>
2016-12-09 11:57:37 -0300

    auth

--- web/controllers/auth_controller.ex
+++ web/controllers/auth_controller.ex
@@ -1,9 +1,18 @@
 defmodule Discuss.AuthController do
     use Discuss.Web, :controller
+    alias Discuss.User
+
     plug Ueberauth
 
 
     def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+        user_params = %{token: auth.credentials.token, email: auth.info.email, provider: "github"}
+        
+        changeset = User.changeset(%User{}, user_params)
+    end
+
+    #   verify if already exists the signed in user on the database
+    defp insert_or_update_user(changeset) do
         
     end
 end
--- web/models/user.ex
+++ web/models/user.ex
@@ -9,10 +9,10 @@ defmodule Discuss.User do
         timestamps()
     end
 
-    def changeset(struct, params \\ &{}) do
+    def changeset(struct, params \\ %{}) do
         struct
         |>  cast(params, [:email, :provider, :token])
         |>  validate_required([:email, :provider, :token])
     end
-    
+
 end
